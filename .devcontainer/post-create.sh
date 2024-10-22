e30659532360b0dfe6bac4855847ff7f30ad797f
Shaju Mohammed <shaju.mohammed@keoic.com>
2024-10-22 10:19:59 +0000

    updated to to be more interactive

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,15 +6,23 @@ echo -e "\e[1;94mYo Version:\e[0m $(yo --version)"
 echo -e "\e[1;94mSPFx Generator Version:\e[0m $(npm list -g @microsoft/generator-sharepoint --depth=0 | sed -n 's/.*@microsoft\/generator-sharepoint@\([^\s]*\).*/\1/p')"
 
 if [ ! -f package.json ]; then
-  read -p "Are you provisioning a new project? (y/n): " answer
+  read -p "Do you want to provision a new project? (y/n): " answer
   if [[ "$answer" =~ ^[Yy]$ ]]; then
     yo @microsoft/sharepoint
+  else
+    echo "When you are ready, run 'bash .devcontainer/post-create.sh' to proceed."
+    exit 0
   fi
 fi
-echo -e "\n\e[1;94mNote:\e[0m To use the 'gulp serve' command, you need to provision certificates and copy and install them locally."
-read -p "Do you want to provision certificates? (y/n): " cert_answer
-if [[ "$cert_answer" =~ ^[Yy]$ ]]; then
-  bash ./.devcontainer/spfx-startup.sh
+
+if [ -f package.json ]; then
+  echo -e "\n\e[1;94mNote:\e[0m To use the 'gulp serve' command, you need to provision certificates and copy and install them locally."
+  read -p "Do you want to provision certificates? (y/n): " cert_answer
+  if [[ "$cert_answer" =~ ^[Yy]$ ]]; then
+    bash ./.devcontainer/spfx-startup.sh
+  else
+    echo "When you are ready, run 'bash ./.devcontainer/spfx-startup.sh' to provision certificates."
+  fi
 fi
 
 
